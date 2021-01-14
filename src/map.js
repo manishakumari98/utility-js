55ae9ee04d86b6307d5c0c81ccc6a56bb5d519c3
manishakumari98 <mk98.manisha@gmail.com>
2021-01-14 22:46:07 +0530

    Replace iterative with recursive implementation of map function, All test cases pass

--- src/map.js
+++ src/map.js
@@ -1,10 +1,21 @@
+const head = require('./head');
+const tail = require('./tail');
+
 const map = (arr, appliedFunction) => {
 	let result = [];
-	for(let i=0; i<arr.length; i++){
-		let mappedValue = appliedFunction(arr[i]);
-		result.push(mappedValue);
-	}
+	result = mapFunction(arr, appliedFunction, result);
 	return result;
+}
+
+function mapFunction(arr, appliedFunction, result){
+	if(arr.length == 0){
+		return result;
+	}else{
+		let mappedValue = appliedFunction(head(arr));
+		result.push(mappedValue);
+
+		return mapFunction(tail(arr), appliedFunction, result);
+	}
 }
 
 module.exports = map;
